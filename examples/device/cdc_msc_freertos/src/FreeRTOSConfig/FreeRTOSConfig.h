44eec3e8c03683566e90144912f7e59102927a99
Nikhil Nayyar <nikhil.sanjiv.nayyar@gmail.com>
2024-06-27 12:09:17 -0400

    fixup! Update CDC_MSC example to work for DeVito SAMD51N19A

--- examples/device/cdc_msc_freertos/src/FreeRTOSConfig/FreeRTOSConfig.h
+++ examples/device/cdc_msc_freertos/src/FreeRTOSConfig/FreeRTOSConfig.h
@@ -45,13 +45,20 @@ void assert_triggered( const char *file, uint32_t line );
 #endif
 #include <peripheral_clk_config.h>
 
+//--------------------------------------------------------------------+
+// RTOS System Settings
+//--------------------------------------------------------------------+
+
 #define configCPU_CLOCK_HZ ( CONF_CPU_FREQUENCY )
 #define configTICK_RATE_HZ ( (portTickType)1000 )
-#define configMINIMAL_STACK_SIZE ( (unsigned short) 1024 )
+#define configMINIMAL_STACK_SIZE ( (unsigned short) 128 )
 #define configTOTAL_HEAP_SIZE ( ( size_t )( 120000 ) )
 #define configMAX_TASK_NAME_LEN ( 16 )
 #define configQUEUE_REGISTRY_SIZE 10
 
+//--------------------------------------------------------------------+
+// RTOS Feature Settings
+//--------------------------------------------------------------------+
 #define configUSE_PREEMPTION 1
 #define configUSE_TIME_SLICING 1
 #define configUSE_IDLE_HOOK 1
@@ -61,16 +68,48 @@ void assert_triggered( const char *file, uint32_t line );
 #define configUSE_MUTEXES 1
 #define configUSE_RECURSIVE_MUTEXES 1
 #define configUSE_COUNTING_SEMAPHORES           1
+#define configUSE_DAEMON_TASK_STARTUP_HOOK 1
 #define configRECORD_STACK_HIGH_ADDRESS 1
 #define configUSE_TASK_NOTIFICATIONS 1
 #define configIDLE_SHOULD_YIELD 1
-
+//https://www.freertos.org/Pend-on-multiple-rtos-objects.html
+#define configUSE_QUEUE_SETS 1
  
 #define configUSE_TICK_HOOK 0
 #define configUSE_16_BIT_TICKS 0
 
+
+//--------------------------------------------------------------------+
+// Priority Settings
+// See https://www.freertos.org/RTOS-Cortex-M3-M4.html before making
+// any changes!
+//--------------------------------------------------------------------+
+
 // // SAMD51 has only 8 priority levels
 #define configPRIO_BITS 3
+
+// 9 Priorities on SAMD51
+#define configMAX_PRIORITIES ( 9 )
+//#define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
+
+/* The lowest interrupt priority that can be used in a call to a "set priority"
+function. */
+#define configLIBRARY_LOWEST_INTERRUPT_PRIORITY 7
+
+/* The highest interrupt priority that can be used by any interrupt service
+routine that makes calls to interrupt safe FreeRTOS API functions.  DO NOT CALL
+INTERRUPT SAFE FREERTOS API FUNCTIONS FROM ANY INTERRUPT THAT HAS A HIGHER
+PRIORITY THAN THIS! (higher priorities are lower numeric values. */
+#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY 4
+
+/* Interrupt priorities used by the kernel port layer itself.  These are generic
+to all Cortex-M ports, and do not rely on any particular library functions. */
+#define configKERNEL_INTERRUPT_PRIORITY (configLIBRARY_LOWEST_INTERRUPT_PRIORITY << (8 - configPRIO_BITS))
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY (configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY << (8 - configPRIO_BITS))
+
+//--------------------------------------------------------------------+
+// Other Settings
+//--------------------------------------------------------------------+
 
 // https://www.freertos.org/Stacks-and-stack-overflow-checking.html
 #define configCHECK_FOR_STACK_OVERFLOW 2
@@ -80,8 +119,6 @@ void assert_triggered( const char *file, uint32_t line );
 
 #define configUSE_MALLOC_FAILED_HOOK 0
 
-//https://www.freertos.org/Pend-on-multiple-rtos-objects.html
-#define configUSE_QUEUE_SETS 1
 #define configENABLE_BACKWARD_COMPATIBILITY 1
 
 // Generate runtime stats
@@ -90,10 +127,6 @@ void assert_triggered( const char *file, uint32_t line );
 
 /* Co-routine definitions. */
 #define configUSE_CO_ROUTINES 0
-
-// 9 Priorities on SAMD51
-#define configMAX_PRIORITIES ( 5 )
-#define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
 
 /* Software timer definitions. */
 #define configUSE_TIMERS 1
@@ -113,7 +146,6 @@ void assert_triggered( const char *file, uint32_t line );
 #ifndef TIMER_TASK_STACK_DEPTH
 #define configTIMER_TASK_STACK_DEPTH (64)
 #endif
-
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
@@ -145,8 +177,8 @@ header file. */
 
 /* Definitions that map the FreeRTOS port interrupt handlers to their CMSIS
 standard names - or at least those used in the unmodified vector table. */
-#define xPortPendSVHandler PendSV_Handler
 #define vPortSVCHandler SVC_Handler
+#define xPortPendSVHandler PendSV_Handler
 #define xPortSysTickHandler SysTick_Handler
 
 
@@ -157,26 +189,6 @@ extern uint32_t vGetRunTimeCounterValue(void);
 #define portCONFIGURE_TIMER_FOR_RUN_TIME_STATS()
 #define portGET_RUN_TIME_COUNTER_VALUE()  xTaskGetTickCount()
 #endif
-
-
-
-/* The lowest interrupt priority that can be used in a call to a "set priority"
-function. */
-#define configLIBRARY_LOWEST_INTERRUPT_PRIORITY 0x07
-
-/* The highest interrupt priority that can be used by any interrupt service
-routine that makes calls to interrupt safe FreeRTOS API functions.  DO NOT CALL
-INTERRUPT SAFE FREERTOS API FUNCTIONS FROM ANY INTERRUPT THAT HAS A HIGHER
-PRIORITY THAN THIS! (higher priorities are lower numeric values. */
-#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY	2
-
-/* Interrupt priorities used by the kernel port layer itself.  These are generic
-to all Cortex-M ports, and do not rely on any particular library functions. */
-#define configKERNEL_INTERRUPT_PRIORITY (configLIBRARY_LOWEST_INTERRUPT_PRIORITY << (8 - configPRIO_BITS))
-#define configMAX_SYSCALL_INTERRUPT_PRIORITY (configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY << (8 - configPRIO_BITS))
-
-
-
 
 // Added by Voytek for CLI support
 #define configCOMMAND_INT_MAX_OUTPUT_SIZE 512
--- hw/bsp/samd5x_e5x/boards/metro_m4_express/board.mk
+++ hw/bsp/samd5x_e5x/boards/metro_m4_express/board.mk
@@ -6,6 +6,8 @@ CFLAGS += -D__SAMD51N19A__
 LD_FILE = $(BOARD_PATH)/$(BOARD).ld
 
 # For flash-jlink target
+# JLINK_DEVICE = ATSAMD51J19
+# Updated by Nikhil to allow spoofing of this file by DeVito BUCB
 JLINK_DEVICE = ATSAMD51N19
 
 flash: flash-bossac
